cc47cf4f0e8137bd4a77d47a79b745e8bfcc9d4e
Anat Hewari <anat.hewa@gmail.com>
2021-07-13 14:40:03 +0300

    Save form values upon refresh

--- complaintform.php
+++ complaintform.php
@@ -43,35 +43,35 @@
                 <span class="error form-control-sm"><?php echo $menu_err; ?></span>
                 <select class="form-select" id="menu" aria-label="Default select example" name="menu">
                     <option disabled selected hidden>Select</option>
-                    <option value="payment failed">Payment Failed</option>
-                    <option value="refund">Refund</option>
-                    <option value="other">Other</option>
+                    <option <?php if ($_POST["menu"] == "payment failed") { ?> selected="true" <?php }; ?> value="payment failed">Payment Failed</option>
+                    <option <?php if ($_POST["menu"] == "refund") { ?> selected="true" <?php }; ?> value="refund">Refund</option>
+                    <option <?php if ($_POST["menu"] == "other") { ?> selected="true" <?php }; ?> value="other">Other</option>
                 </select>
             </div>
 
             <div class="form-group">
                 <label for="description">Description</label>
                 <span class="error form-control-sm"><?php echo $description_err; ?></span>
-                <textarea class="form-control" name="description" id="description" rows="3" value="<?php echo $description; ?>"></textarea>
+                <textarea class="form-control" name="description" id="description" rows="3" value="<?php echo $description; ?>"><?php echo $_POST["description"]; ?></textarea>
             </div>
 
             <div class="form-group">
                 <label for="method">Payment Method:</label>
                 <span class="error form-control-sm"><?php echo $method_err; ?></span>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="method" id="method1" value="mastercard">
+                    <input class="form-check-input" type="radio" name="method" id="method1" <?php if ($_POST["method"] == "mastercard") { ?> checked="true" <?php }; ?> value="mastercard">
                     <label class="form-check-label" for="method1">
                         MasterCard
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="method" id="method2"  value="visa">
+                    <input class="form-check-input" type="radio" name="method" id="method2" <?php if ($_POST["method"] == "visa") { ?> checked="true" <?php }; ?> value="visa">
                     <label class="form-check-label" for="method2">
                         Visa
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" name="method" id="method3" value="cash">
+                    <input class="form-check-input" type="radio" name="method" id="method3" <?php if ($_POST["method"] == "cash") { ?> checked="true" <?php }; ?> value="cash">
                     <label class="form-check-label" for="method3">
                         Cash
                     </label>
@@ -82,13 +82,13 @@
                 <label for="urgent">Urgent:</label>
                 <span class="error form-control-sm"><?php echo $urgent_err; ?></span>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" value="Y" id="urgent" name="urgent">
+                    <input class="form-check-input" type="radio" value="Y" id="urgent" <?php if ($_POST["urgent"] == "Y") { ?> checked="true" <?php }; ?> name="urgent">
                     <label class="form-check-label" for="urgent">
                         Yes
                     </label>
                 </div>
                 <div class="form-check">
-                    <input class="form-check-input" type="radio" value="N" id="urgent" name="urgent">
+                    <input class="form-check-input" type="radio" value="N" id="urgent" <?php if ($_POST["urgent"] == "N") { ?> checked="true" <?php }; ?>  name="urgent">
                     <label class="form-check-label" for="urgent">
                         No
                     </label>
